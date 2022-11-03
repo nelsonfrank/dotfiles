a4bdb93ede44828453f64b992c1f0ab8030c46b1
nelson frank <nelsonfr78@outlook.com>
2022-11-03 21:46:34 +0300

    feat: Add comment in rofi-powermenu files to help setup

--- rofi/rofi-powermenu.sh
+++ rofi/rofi-powermenu.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
+#########################
+# INSTRUCTIONS
+#########################
+
+# 1. Copy this file to ~/.local/bin/powermenu
+# 2. then change permission of powermenu file to executable
+# i.e sudo chmod +e ~/.local/bin/powermenu
+# 
+# 3. Test powermenu command in your terminal to see if it open rofi powermenu options
+
+##########################
+
+
 chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
